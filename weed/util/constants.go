69bcbed4f363d97266a1b14150f8f6e53adb8a37
zemul <zemul@foxmail.com>
2024-04-01 15:58:35 +0800

    fix: S3 PORT

--- weed/util/constants.go
+++ weed/util/constants.go
@@ -8,7 +8,7 @@ var (
 	VERSION_NUMBER = fmt.Sprintf("%.02f", 3.57)
 	VERSION        = sizeLimit + " " + VERSION_NUMBER
 	COMMIT         = ""
-	TAG            = "2023.11.02"
+	TAG            = "2023.11.02-fix-s3port"
 )
 
 func Version() string {
